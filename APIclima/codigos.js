c45b450783d1c7966377c29c13c3c78152f4c580
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2024-04-26 18:13:37 -0300

    Add files via upload

--- APIclima/codigos.js
+++ APIclima/codigos.js
@@ -1,5 +1,4 @@
-//https://api.openweathermap.org/data/2.5/weather?q=Paulista&appid=f37d3286de44c3a9171ecb8e55d07280&lang=pt_br
-
+//https://api.openweathermap.org/data/2.5/weather?q=Paulista&units=metric&appid=f37d3286de44c3a9171ecb8e55d07280&lang=pt_br
 
 //Variáveis e Seleções de Elementos 
 const armazenaCidade = document.querySelector("#city-input");
@@ -7,7 +6,7 @@ const Btpesquisa = document.querySelector("#pesquisa");
 const chaveAPI = "f37d3286de44c3a9171ecb8e55d07280";
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperatura span");
-const descElement = document.querySelector("#descricaocontainer");
+const descElement = document.querySelector("#climatempo");
 const weatherIconElement = document.querySelector("#weather-icon");
 const umidityElement = document.querySelector("#umidade span");
 const windElement = document.querySelector("#vento span");
@@ -16,16 +15,24 @@ const windElement = document.querySelector("#vento span");
 //Funções
 
 const pegaDadosTempo = async(cidade) => {
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chaveAPI}&lang=pt_br`;
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${chaveAPI}&lang=pt_br`;
 
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
-    console.log(data);
+    
+    return data;
 };
 
-const mostraDadosTempo = (cidade) => {
-    console.log(cidade);
-    pegaDadosTempo(cidade);
+const mostraDadosTempo = async(cidade) => {
+    const data = await pegaDadosTempo(cidade);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    umidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
 };
 
 //Eventos
