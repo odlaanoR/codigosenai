eb64e816c7179bed56ed898c7ece03cf2e4275b1
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2024-08-05 17:28:33 -0300

    Add files via upload

---
+++ ronluis3e/recebeaula07.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>link enviado</title>
+</head>
+<body>
+    <?php
+
+    echo "Olá ", $_GET["nome"], " seja bem-vindo ao sistema!!";
+    //método GET: é possível enviar parâmetros, com a concatenação:
+    //o padrão do GET é: url?par1=valor?
+
+?>
+</body>
+</html>
