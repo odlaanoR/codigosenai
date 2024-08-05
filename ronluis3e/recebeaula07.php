4847b7f19012d28491a3b4b882328000212c9b3d
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2024-08-05 18:21:27 -0300

    Add files via upload

--- ronluis3e/recebeaula07.php
+++ ronluis3e/recebeaula07.php
@@ -8,10 +8,13 @@
 <body>
     <?php
 
-    echo "Olá ", $_GET["nome"], " seja bem-vindo ao sistema!!";
+    echo "Olá ", $_GET["nome"], " ", $_GET["sobrenome"], " seja bem-vindo ao sistema! <br>";
     //método GET: é possível enviar parâmetros, com a concatenação:
     //o padrão do GET é: url?par1=valor?
-
+    //esse arquivo não abre diretamente, precisa abrir o html antes, por conta do parâmetro, pra ele ler o parâmetro e abrir o arquivo
 ?>
+
+    <a href="aula07.html">Voltar pra página Inicial</a>
+
 </body>
 </html>
