abf3b9bd44b1cdbc6f4f4066ca596334cc70c7e9
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2024-10-22 18:15:07 -0300

    Update and rename index.php to form-cadastra-leitores.php

--- Aulasphp/biblioteca_database/form-cadastra-leitores.php
+++ Aulasphp/biblioteca_database/form-cadastra-leitores.php
@@ -4,32 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca database</title>
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="estilo-cadastro.css">
 </head>
 <body>
     <header>
         <h1>Biblioteca poggers</h1>
     </header>
-    <p>Seja bem-vindo a biblioteca los zokas, aqui vc encontrará de tudo que tem para ler! Confira as opções:</p>
-    <?php
-
-        echo "<table class='livros'><tr>";
-            for ($x = 1;  $x <= 5; $x++) {
-            echo "<td><img src='imgs/$x.jpg'></td>";
-
-        }
-        echo "</tr>";
-        echo "<tr>";
-            for ($x = 6; $x <= 10; $x++) {
-            echo "<td><img src='imgs/$x.jpg'></td>";
-        }
-        echo "</tr></table>";
-
-
-    ?>
-
-
-    <p>Gostou de algum deles? Faça o cadastro abaixo para conseguir alugar qualquer um desses livros e desfrutar de suas histórias! </p>
+    <p>Preencha os campos abaixo, de acordo com os dados, corretamente.</p>
         <form action="cadastra-leitores.php" method="post">
             <table class="cadastro">
                 <tr>
